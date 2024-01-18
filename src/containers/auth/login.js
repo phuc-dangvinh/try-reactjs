b714d262a50be46d993732b853cf8a1ea1ebc8c5
Dang Vinh Phuc <phucdang@trulet.com>
2024-01-18 16:52:33 +0700

    #34

--- src/containers/auth/login.js
+++ src/containers/auth/login.js
@@ -9,10 +9,104 @@ import "./login.scss";
 class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      username: "",
+      password: "",
+      isShowPassword: false,
+    };
   }
 
+  handleOnChangeUsername = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+
+  handleOnChangePassword = (event) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  handleLogin = () => {
+    console.log(this.state);
+  };
+
+  handleShowHidePassword = () => {
+    this.setState({
+      isShowPassword: !this.state.isShowPassword,
+    });
+  };
+
   render() {
-    return <div>Hello login</div>;
+    return (
+      <div className="login-background">
+        <div className="login-container">
+          <div className="login-content row">
+            <div className="col-12 text-login">Login</div>
+            <div className="col-12 forn-group login-input">
+              <label>Username:</label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your username"
+                value={this.state.username}
+                onChange={(event) => {
+                  this.handleOnChangeUsername(event);
+                }}
+              />
+            </div>
+            <div className="col-12 forn-group login-input">
+              <label>Password:</label>
+              <div className="custom-input-password">
+                <input
+                  className="form-control"
+                  type={this.state.isShowPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  value={this.state.password}
+                  onChange={(event) => {
+                    this.handleOnChangePassword(event);
+                  }}
+                />
+                <span
+                  onClick={() => {
+                    this.handleShowHidePassword();
+                  }}
+                >
+                  <i
+                    class={
+                      this.state.isShowPassword
+                        ? "far fa-eye"
+                        : "far fa-eye-slash"
+                    }
+                  ></i>
+                </span>
+              </div>
+            </div>
+            <div className="col-12">
+              <button
+                className="btn-login"
+                onClick={() => {
+                  this.handleLogin();
+                }}
+              >
+                Login
+              </button>
+            </div>
+            <div className="col-12">
+              <span className="forgot-password">Forget your password?</span>
+            </div>
+            <div className="col-12 text-center mt-3">
+              <span className="text-other-login">Or login with:</span>
+            </div>
+            <div className="col-12 social-login">
+              <i className="fab fa-google-plus-g google"></i>
+              <i className="fab fa-facebook-f facebook"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
